fa227b8eac4c95e45c376c61fdfcac5fb2e82b6d
AllHailMegatron <ahmadrthomp@gmail.com>
2019-05-23 21:34:40 -0400

    Completed Tabs.js file

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -19,10 +19,11 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content.classList.toggle(".dropdown-hidden");
+    this.content.classList.toggle("dropdown-hidden");
   }
 }
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
+
